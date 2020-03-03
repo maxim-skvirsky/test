2abc3d9d994f97b20c704067b3f8c578a4ba2991
Maxim Skvirsky <maxim.skvirsky@ptts.com>
2020-03-03 15:07:00 +0200

    a

--- script.sh
+++ script.sh
@@ -1,37 +1,37 @@
+ensure_no_confilicts () {
+    out = "$1"
+    if [[ $out == CONFLICT* ]]
+    then
+        # echo $out
+        echo "To continue - please resolve and commit merge conflicts in the GIT panel."
+        merge_state=$(git diff --full-index && git diff-index HEAD)
+        info="waiting for conflicts to be resolved and commited..."
+        delay=0.2
+        spinstr='|/-\'
+        echo $info
+        while [ ! -z "$merge_state" ]; do
+            tput civis
+            temp=${spinstr#?}
+            printf "[%c]" "$spinstr"
+            spinstr=$temp${spinstr%"$temp"}
+            sleep $delay
+            reset="\b\b\b"
+            printf $reset
+            merge_state=$(git diff --full-index && git diff-index HEAD)
+            if [[ -z "$merge_state" ]]; then
+                echo "[done]"
+            fi
+        done
+        tput cnorm
+        echo ""
+        echo "Conflicts resolved. Continuing"
+    else
+        echo "all gewd"
+    fi
+    
+    echo "DONE"
+}
+
 git branch -D TEMP
 git checkout -b TEMP
-out=$(git merge master)
-if [[ $out == CONFLICT* ]]
-then
-    # echo $out
-    echo "Please fix merge conflicts, the script will resume once conflicts are resolved and commited."
-    merge_state=$(git diff --full-index && git diff-index HEAD)
-    info="waiting for conflicts to be resolved ..."
-    delay=0.2
-    spinstr='|/-\'
-    echo $info
-    while [ ! -z "$merge_state" ]; do
-        tput civis
-        temp=${spinstr#?}
-        printf "[%c]" "$spinstr"
-        spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        reset="\b\b\b"
-        # reset="\b\b\b\b\b\b"
-        # for ((i=1; i<=$(echo $info | wc -c); i++)); do
-        #     reset+="\b"
-        # done
-        printf $reset
-        merge_state=$(git diff --full-index && git diff-index HEAD)
-        if [[ -z "$merge_state" ]]; then
-            echo "[done]"
-        fi
-    done
-    tput cnorm
-    echo ""
-    echo "Conflicts resolved. Continuing"
-else
-    echo "all gewd"
-fi
-
-echo "DONE"
+ensure_no_confilicts $(git merge master)
