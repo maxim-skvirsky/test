fc2095d6c3bc087f72d68807f67cbfed8ccdbee6
Maxim Skvirsky <maxim.skvirsky@ptts.com>
2020-03-04 14:35:58 +0200

    a

--- script.sh
+++ script.sh
@@ -32,12 +32,14 @@ ensure_no_confilicts () {
 }
 git branch -D TEMP
 git checkout -b TEMP
-ensure_no_confilicts $(git merge master)
-git checkout master
-git merge TEMP
-git push
-git checkout $branch
-echo "Done :)"
+message=$(git merge master)
+echo "$message"
+# ensure_no_confilicts $(git merge master)
+# git checkout master
+# git merge TEMP
+# git push
+# git checkout $branch
+# echo "Done :)"
 
 
 # echo "**********************************************************
